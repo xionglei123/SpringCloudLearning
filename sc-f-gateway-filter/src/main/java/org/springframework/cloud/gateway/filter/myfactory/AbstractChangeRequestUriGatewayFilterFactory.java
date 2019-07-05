8199348d025c21d9333374ad2c028d8081c70588
xionglei123 <396283528@qq.com>
2019-07-05 18:17:47 +0800

    springcloud学习之路

---
+++ sc-f-gateway-filter/src/main/java/org/springframework/cloud/gateway/filter/myfactory/AbstractChangeRequestUriGatewayFilterFactory.java
@@ -0,0 +1,16 @@
+package org.springframework.cloud.gateway.filter.myfactory;
+
+import gateway.RequestTimeGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import java.util.List;
+
+/**
+ * Email miles02@163.com
+ *
+ * @author fangzhipeng
+ * create 2018-12-04
+ **/
+public abstract class AbstractChangeRequestUriGatewayFilterFactory extends AbstractGatewayFilterFactory {
+}
